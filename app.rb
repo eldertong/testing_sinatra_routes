0f2e4f47acfc0d84d41b4f4818cad30252fc091d
JTong <eldertong@gmail.com>
2017-02-20 12:39:16 -0500

    adding details to app

---
+++ app.rb
@@ -0,0 +1,5 @@
+require 'sinatra'
+
+class PersonalDetailsApp < Sinatra::Base
+
+end
--- test/test_app.rb
+++ test/test_app.rb
@@ -3,9 +3,10 @@ require 'rack/test'
 require_relative '../app.rb'
 
 class TestApp < Minitest::Test
+    include Rack::Test::Methods
 
     def test_ask_name_on_entry_page
-    get '/'
-    assert(last_response.ok?)
+        get '/'
+        assert(last_response.ok?)
     end
 end
