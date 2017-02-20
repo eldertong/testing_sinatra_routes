2a01bf0f1538a9075a4123252f5561c04c68628c
JTong <eldertong@gmail.com>
2017-02-20 12:30:50 -0500

    initial commit

---
+++ test/test_app.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'rack/test'
+require_relative '../app.rb'
+
+class TestApp < Minitest::Test
+
+    def test_ask_name_on_entry_page
+    get '/'
+    assert(last_response.ok?)
+    
+end
